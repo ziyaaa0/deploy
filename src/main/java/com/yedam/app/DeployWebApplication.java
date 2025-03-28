626be1b9ad11895e112af78dd9bb0b22d01610f7
ziyaaa0 <jhee97e@gmail.com>
2025-03-28 10:33:20 +0900

    first commit

---
+++ src/main/java/com/yedam/app/DeployWebApplication.java
@@ -0,0 +1,13 @@
+package com.yedam.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployWebApplication.class, args);
+	}
+
+}
